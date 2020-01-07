ed8603aff44796f65a327da03f0f34e5c226d6f3
wdwatkins <wwatkins@usgs.gov>
2020-01-06 16:12:39 -0800

    pull from mesonet w/scipiper

--- 1_fetch/src/download_data.R
+++ 1_fetch/src/download_data.R
@@ -2,10 +2,15 @@ download_mesonet_data <- function(target_name, nws_site, dateTime_str) {
   
   # Some dummy stuff for now
   dateTime <- as.POSIXct(dateTime_str, format = "%Y%m%d_%H")
-  meso_date <- format(dateTime, "%Y%m%d")
+  meso_date <- format(dateTime, "%Y-%m-%d")
   meso_time <- format(dateTime, "%H%M")
-  meso_data <- sprintf("NWS Site %s at %s : %s", nws_site, meso_date, meso_time)
+  message(sprintf("NWS Site %s at %s : %s", nws_site, meso_date, meso_time))
+  #note that a double percent is required before the date here in order to escape the percent sign 
+  #Will get a "not enough arguments" error otherwise
+  url <- sprintf("https://mesonet.agron.iastate.edu/plotting/auto/plot/160/station:%s::dt:%s%%20%s::var:primary::dpi:100.csv",
+                 nws_site, meso_date, meso_time)
   
+  meso_data <- read_csv(url, col_types = list(issued = col_character()))
   saveRDS(meso_data, file = target_name)
   
 }
